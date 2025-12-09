9e0b4078d2f5fa390c1f0b6421ccd208d53ce7cc
Alexandr <aleksandrivanov20208@gmail.com>
2025-12-09 22:24:08 +0300

    Started development of app, that can display weather in Moscow(and other cities too, if i don't be so lazy)

---
+++ Chapter1_C#/WeatherClient/ClientOfAPI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace WeatherClient
+{
+    class ClientOfAPI
+    {
+        private HttpClient _client;
+        private string _apiKey = "99f3178ce572598bb30637ab56b96754";
+        private string _moscowId = "569842";
+
+        private string _connectionString;
+
+        public ClientOfAPI() 
+        {
+            _client = new HttpClient();
+            _connectionString = $"http://api.openweathermap.org/data/2.5/forecast?id={_moscowId}&appid={_apiKey}";
+        }
+
+        public async Task<string> GetWeatherJson() 
+        {
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync(_connectionString);
+                return response.Content.ToString();
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Chapter1_C#/WeatherClient/JsonWorker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherClient
+{
+    class JsonWorker
+    {
+        ClientOfAPI api;
+
+        public JsonWorker()
+        {
+            api = new ClientOfAPI();
+        }
+
+        public 
+    }
+}
